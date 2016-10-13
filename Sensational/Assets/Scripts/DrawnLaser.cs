2ffe2cf93fab88f78e704009e6b6c0eb758bfcbc
winthos <winson.whan@gmail.com>
2016-10-13 10:45:53 -0700

    backup 1

---
+++ Sensational/Assets/Scripts/DrawnLaser.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawnLaser : MonoBehaviour
+{
+    //draws static magenta line between the points
+    private LineRenderer lineRenderer;
+
+    public Transform Point1;
+    public Transform Point2;
+    public Transform Point3;
+    public Transform Point4;
+
+  
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetWidth(0.2f, 0.2f);
+
+        lineRenderer.SetPosition(0, Point1.position);
+        lineRenderer.SetPosition(1, Point2.position);
+        lineRenderer.SetPosition(2, Point3.position);
+        lineRenderer.SetPosition(3, Point4.position);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Sensational/Assets/Scripts/LevelGlobals.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGlobals : MonoBehaviour 
+{
+    public bool TimeStopped = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if (Input.GetMouseButtonDown(1) && TimeStopped == false)
+        {
+            TimeStopped = true;
+        }
+        else if(Input.GetMouseButtonDown(1) && TimeStopped == true)
+        {
+            TimeStopped = false;
+        }
+	}
+}
